0b49c408ebed4f7cb629a2b355857dc71732fdae
serhatbek <serhat.bekk@gmail.com>
2021-01-26 01:16:53 +0300

    styling done

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,10 @@
 const accordionItems = document.querySelectorAll('.accordion__item');
+const accordionHeading = document.querySelector('.accordion__item-title');
+const accordionContent = document.querySelector('.accordion__item-cont');
+
+// accordionItems.forEach((item) => {
+//   item.addEventListener('click', () => {
+//     accordionHeading.classList.toggle('active');
+//     accordionContent.classList.toggle('active');
+//   });
+// });
