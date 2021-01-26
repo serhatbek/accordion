423db040600d07440b7dbc1f20b5bb23af0372e1
serhatbek <serhat.bekk@gmail.com>
2021-01-26 04:07:55 +0300

    last commit

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,9 +2,21 @@ const accordionItems = document.querySelectorAll('.accordion__item');
 const accordionHeading = document.querySelector('.accordion__item-title');
 const accordionContent = document.querySelector('.accordion__item-cont');
 
-// accordionItems.forEach((item) => {
-//   item.addEventListener('click', () => {
-//     accordionHeading.classList.toggle('active');
-//     accordionContent.classList.toggle('active');
-//   });
-// });
+accordionItems.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    showAccPanel(e);
+  });
+});
+
+function showAccPanel(e) {
+  let target = e.currentTarget;
+
+  if (!target.classList.contains('active')) {
+    accordionItems.forEach((item) => {
+      item.classList.remove('active');
+    });
+    target.classList.toggle('active');
+  } else {
+    target.classList.remove('active');
+  }
+}
